2749d58f89843fd7ce3b19d0a77e253af8b6d483
H00gy <148908869+H00gy@users.noreply.github.com>
2026-05-16 15:56:32 -0400

    k

--- Assets/scripts/BookAnimation.cs
+++ Assets/scripts/BookAnimation.cs
@@ -1,26 +1,7 @@
 using UnityEngine;
-using System.Collections;
+
 
 public class BookAnimation : MonoBehaviour
 {
-   public IEnumerator AnimateBookToShelf(Vector2 startPos, Vector2 targetPos, float duration)
-    {
-        float elapsedTime = 0f;
-
-        while (elapsedTime < duration)
-        {
-            if ( this == null) yield break;
-
-            float t = elapsedTime / duration;
-
-            Vector2 basePos = Vector2.Lerp(startPos, startPos + targetPos, t);
-
-            transform.position = new Vector2(basePos.x, basePos.y);
-
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        
-   }
+  
 }
